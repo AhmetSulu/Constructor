26a4c04a2ff234a82c3fd1b8b8c7e1faaf89e33c
Ahmet Sulu <ahmet.sulu1993@gmail.com>
2024-07-30 19:29:49 +0300

    Add project files.

---
+++ Constructor/Constructor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Baby
+    {
+        // Private alanlar (fields)
+        private string _ad;
+        private string _soyad;
+        private DateTime _dogumTarihi;
+
+        // Public özellikler (properties)
+        public string Name
+        {
+            get { return _ad; }
+            set { _ad = value; }
+        }
+
+        public string Surname
+        {
+            get { return _soyad; }
+            set { _soyad = value; }
+        }
+
+        public DateTime BirthDay
+        {
+            get { return _dogumTarihi; }
+            set { _dogumTarihi = value; }
+        }
+
+        // Parametresiz kurucu metot (constructor)
+        public Baby()
+        {
+            Default(); // Varsayılan değerleri ayarla
+        }
+
+        // Parametreli kurucu metot (constructor)
+        public Baby(string name, string surname)
+        {
+            Default(); // Varsayılan değerleri ayarla
+            _ad = name; // İsim değerini ata
+            _soyad = surname; // Soyisim değerini ata
+        }
+
+        // Varsayılan değerleri ayarlayan özel metot (method)
+        private void Default()
+        {
+            System.Console.WriteLine("Ingaaaa"); // Konsola bir mesaj yazdır
+            _dogumTarihi = DateTime.Now; // Doğum tarihini şimdiki zaman olarak ayarla
+        }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,11 @@
+﻿using Constructor;
+
+// Default constructor ile nesne oluşturma
+Baby bebek1 = new Baby();
+Console.WriteLine("Bebek 1:");
+Console.WriteLine($"Ad: {bebek1.Name} \nSoyad: {bebek1.Surname} \nDoğum Tarihi: {bebek1.BirthDay} ");
+
+// Ad ve Soyad parametrelerini alan constructor ile nesne oluşturma
+Baby bebek2 = new Baby("Muhammed Hamza", "Yuyucu");
+Console.WriteLine("\nBebek 2:");
+Console.WriteLine($"Ad: {bebek2.Name} \nSoyad: {bebek2.Surname} \nDoğum Tarihi: {bebek2.BirthDay}");
